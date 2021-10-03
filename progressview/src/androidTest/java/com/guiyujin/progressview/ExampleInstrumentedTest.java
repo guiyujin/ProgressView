a7f710e283188fa54c9ce9211a622e56339a0d74
guiyujin <ye3978@gmail.com>
2021-10-03 14:16:49 +0800

    github

--- progressview/src/androidTest/java/com/guiyujin/progressview/ExampleInstrumentedTest.java
+++ progressview/src/androidTest/java/com/guiyujin/progressview/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.guiyujin.processview.test", appContext.getPackageName());
+        assertEquals("com.guiyujin.progressview.test", appContext.getPackageName());
     }
 }
